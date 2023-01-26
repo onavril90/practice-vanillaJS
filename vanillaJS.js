7fb65c2296873e91a09d2026c4e52e253f0873e8
Sewon Kim <boowoo90@gmail.com>
2023-01-26 10:50:53 +0900

    first coomit

---
+++ vanillaJS.js
@@ -0,0 +1,15 @@
+const data1 = [1, 2, "Dave", null];
+
+const data2 = [
+  { name: "Jane", age: 30},
+  { name: "Alex", age: 40}
+];
+
+const data3 = [
+  [1, 2, 3],
+  [2, 3, 4]
+];
+
+console.log(data1[0]);
+console.log(data2[0].name);
+console.log(data3[0][2]);
